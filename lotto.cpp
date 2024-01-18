80c443a668fdf4f25f21b106133ec7166e669d35
minichip3 <mini@minichip.net>
2024-01-18 22:30:16 +0900

    first commit

---
+++ lotto.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    vector<int> nums;
+    for (int i = 0; i < 6; i++)
+    {
+        int num = rand() % 45 + 1;
+        while (find(nums.begin(), nums.end(), num) != nums.end())
+        {
+            num = rand() % 45 + 1;
+        }
+        nums.push_back(num);
+    }
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (nums[j] > nums[j+1])
+            {
+                swap(nums[j], nums[j+1]);
+            }
+        }
+    }
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
